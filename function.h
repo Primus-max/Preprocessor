e301bd06d56d8552aa7f40fc1f193c446ee68eb0
Vladimir <79203161+Primus-max@users.noreply.github.com>
2024-05-06 14:11:17 +0300

    Update function.h
    Убрал комментарии

--- function.h
+++ function.h
@@ -4,15 +4,15 @@
 //#define PRINT_ARRAY printArray_Int
 //
 //#ifdef INTEGER
-//// Другие обобщенные имена функций для целых чисел...
+
 //#elif defined(DOUBLE)
 //#define FILL_ARRAY fillArray_Double
 //#define PRINT_ARRAY printArray_Double
-//// Другие обобщенные имена функций для действительных чисел...
+
 //#elif defined(CHAR)
 //#define FILL_ARRAY fillArray_Char
 //#define PRINT_ARRAY printArray_Char
-//// Другие обобщенные имена функций для символов...
+
 //#endif
 
 #define FILL_ARRAY(arr, size, type) \
@@ -30,37 +30,37 @@
 #define EDIT_ELEMENT(arr, size, type, position, element) \
     editElementToArray_##type(arr, size, position, element);
 
-#pragma region Заполнение массива
+#pragma region Г‡Г ГЇГ®Г«Г­ГҐГ­ГЁГҐ Г¬Г Г±Г±ГЁГўГ 
 void fillArray_Int(int* arr, int size);
 void fillArray_Double(double* arr, int size);
 void fillArray_Char(char* arr, int size);
 #pragma endregion
 
-#pragma region Печать массива
+#pragma region ГЏГҐГ·Г ГІГј Г¬Г Г±Г±ГЁГўГ 
 void printArray_Int(int* arr, int size);
 void printArray_Double(double* arr, int size);
 void printArray_Char(char* arr, int size);
 #pragma endregion
 
-#pragma region Поиск минимального
+#pragma region ГЏГ®ГЁГ±ГЄ Г¬ГЁГ­ГЁГ¬Г Г«ГјГ­Г®ГЈГ®
 int findMinimal_Int(int* arr, int size);
 double findMinimal_Double(double* arr, int size);
 char findMinimal_Char(char* arr, int size);
 #pragma endregion
 
-#pragma region Поиск максимального
+#pragma region ГЏГ®ГЁГ±ГЄ Г¬Г ГЄГ±ГЁГ¬Г Г«ГјГ­Г®ГЈГ®
 int findMaximum_Int(int* arr, int size);
 double findMaximum_Double(double* arr, int size);
 char findMaximum_Char(char* arr, int size);
 #pragma endregion
 
-#pragma region Сортировка
+#pragma region Г‘Г®Г°ГІГЁГ°Г®ГўГЄГ 
 void sortArray_Int(int* arr, int size);
 void sortArray_Double(double* arr, int size);
 void sortArray_Char(char* arr, int size);
 #pragma endregion
 
-#pragma region Редактирование элемента
+#pragma region ГђГҐГ¤Г ГЄГІГЁГ°Г®ГўГ Г­ГЁГҐ ГЅГ«ГҐГ¬ГҐГ­ГІГ 
 void editElementToArray_Int(int* arr, int size, int position, int element);
 void editElementToArray_Double(double* arr, int size, int position, double element);
 void editElementToArray_Char(char* arr, int size, int position, char element);
